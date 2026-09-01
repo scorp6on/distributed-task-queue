85df7b933af22d35fa274ef670c169c9e6cd6fb0
scorp6on <nn9m@yahoo.com>
2026-09-01 12:58:25 -0400

    fixed the database connection and made a config file that ensures that the links in the .env file is used as the DATABASE_URL

--- app/db.py
+++ app/db.py
@@ -1,11 +1,16 @@
-import os
+from app.config import settings
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./dev.db")
-
-engine = create_engine(DATABASE_URL)
+engine = create_engine(settings.database_url)
 SessionLocal = sessionmaker(bind=engine, autoflush=False)
 
 class Base(DeclarativeBase):
     pass
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
